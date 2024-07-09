42349ac87c9817ca12ba2af4b0a5598a580a96d0
DreaMeR-17 <pronka_1993@mail.ru>
2024-07-09 12:17:57 +0600

    Update Program.cs
    changed row to rows

--- Pictures/Program.cs
+++ Pictures/Program.cs
@@ -9,12 +9,12 @@ namespace Pictures
             int picturesInRow = 3;
             int allPictures = 52;
 
-            int allFullRow = (allPictures / picturesInRow);
+            int allFullRows = (allPictures / picturesInRow);
             int lostPictures = (allPictures % picturesInRow);
 
             Console.WriteLine("Картинок в одном ряду: " + picturesInRow);
             Console.WriteLine("Всего картинок: " + allPictures);
-            Console.WriteLine("Всего полных рядов: " + allFullRow);
+            Console.WriteLine("Всего полных рядов: " + allFullRows);
             Console.WriteLine("Не вошедшие в полный ряд картинки: " + lostPictures);
         }
     }

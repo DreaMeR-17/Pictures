f195d372e35520313e23c05312a9ffd607425762
DreaMeR <DreaMeR@DREAMER>
2024-07-09 10:34:48 +0600

    Добавьте файлы проекта.

---
+++ Pictures/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pictures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int picturesInRow = 3;
+            int allPictures = 52;
+
+            int allFullRow = (allPictures / picturesInRow);
+            int lostPictures = (allPictures % picturesInRow);
+
+            Console.WriteLine("Картинок в одном ряду: " + picturesInRow);
+            Console.WriteLine("Всего картинок: " + allPictures);
+            Console.WriteLine("Всего полных рядов: " + allFullRow);
+            Console.WriteLine("Не вошедшие в полный ряд картинки: " + lostPictures);
+        }
+    }
+}
